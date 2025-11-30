23661b6180393dfddddc32f0e882fe3deaaf78b0
hendrinaibaho2020-eng <hendrinaibaho2020@gmail.com>
2025-12-01 00:38:43 +0700

    Memulai fungsi login & akses, tapi besok

---
+++ TUBES/login_akses.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h> // Untuk fungsi exit
+
+
+typedef struct {
+    char username[20];
+    char password[20];
+    char role[10]; 
+} User;
